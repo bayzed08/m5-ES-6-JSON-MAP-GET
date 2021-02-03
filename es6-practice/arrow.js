2531b1986edbfca1f6a203fa2dfc8ab42c2b2bde
bayzed08 <gbir.islam@gmail.com>
2021-02-03 23:32:37 +0600

    json load mao

---
+++ es6-practice/arrow.js
@@ -0,0 +1,4 @@
+const doubleIt = num => num * 2;
+console.log(doubleIt(5));
+const add = (x, y) => x + y;
+console.log(add(100,400));
---
+++ es6-practice/template.js
@@ -0,0 +1,9 @@
+let firstname = "bayzed";
+let lastName = "islam";
+//const fullName = firstname + lastName;
+const fullName = `${firstname} ${lastName} is  a   good boy. here is his number ${100+200+200}`;
+console.log(fullName);
+const multiline = `hello
+how are you
+fuck u.`;
+console.log(multiline);
